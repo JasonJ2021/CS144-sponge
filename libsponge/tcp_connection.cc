0f79711e8c2bae7227dd7d5fc0960abf051e1fd7
JasonJ2021 <3200104166@zju.edu.cn>
2022-11-18 21:48:29 +0800

    reassembler exists problems , need to be fixed

--- libsponge/tcp_connection.cc
+++ libsponge/tcp_connection.cc
@@ -1,6 +1,7 @@
 #include "tcp_connection.hh"
 
 #include <iostream>
+#include <limits>
 
 // Dummy implementation of a TCP connection
 
@@ -8,33 +9,168 @@
 // automated checks run by `make check`.
 
 template <typename... Targs>
-void DUMMY_CODE(Targs &&... /* unused */) {}
+void DUMMY_CODE(Targs &&.../* unused */) {}
 
 using namespace std;
 
-size_t TCPConnection::remaining_outbound_capacity() const { return {}; }
+size_t TCPConnection::remaining_outbound_capacity() const { return _sender.stream_in().remaining_capacity(); }
 
-size_t TCPConnection::bytes_in_flight() const { return {}; }
+size_t TCPConnection::bytes_in_flight() const { return _sender.bytes_in_flight(); }
 
-size_t TCPConnection::unassembled_bytes() const { return {}; }
+size_t TCPConnection::unassembled_bytes() const { return _receiver.unassembled_bytes(); }
 
-size_t TCPConnection::time_since_last_segment_received() const { return {}; }
+size_t TCPConnection::time_since_last_segment_received() const { return _time_since_last_segment_received; }
 
-void TCPConnection::segment_received(const TCPSegment &seg) { DUMMY_CODE(seg); }
+void TCPConnection::try_send(){
+    if(!_sender.segments_out().empty()){
+        TCPSegment &segment_sent = _sender.segments_out().front();
+        _sender.segments_out().pop();
+        // 携带上ackno & ACK flags
+        if (_receiver.ackno().has_value()) {
+            segment_sent.header().ack = true;
+            segment_sent.header().ackno = _receiver.ackno().value();
+        }
+        segment_sent.header().win = _receiver.window_size() > std::numeric_limits<uint16_t>::max()
+                                        ? std::numeric_limits<uint16_t>::max()
+                                        : _receiver.window_size();
+        _segments_out.push(segment_sent);
+    }
+}
 
-bool TCPConnection::active() const { return {}; }
+void TCPConnection::segment_received(const TCPSegment &seg) {
+    // 处理RST flag
+    if (!is_active)
+        return;
+    _time_since_last_segment_received = 0;  // 空，无效segment是否算？
+    if (seg.header().rst) {
+        _sender.stream_in().set_error();
+        _receiver.stream_out().set_error();
+        is_active = false;
+        // How to kill the connection?
+        return;
+    }
+    // 把segment交给TCPReceiver , 使其能够查看seqno,syn,payload and fin
+    _receiver.segment_received(seg);
+
+    // 如果ACK flag被设置，告知TCPSender ackno and window_size;
+    if (seg.header().ack) {
+        _sender.ack_received(seg.header().ackno, seg.header().win);
+    }
+    // 如果到达的segment拥有任何seqno，TCPConnection保证有一个segment被发送
+    bool should_sent = false;
+    if (seg.length_in_sequence_space() != 0) {
+        _sender.fill_window();
+        should_sent = true;
+        if (_sender.segments_out().empty()) {
+            _sender.send_empty_segment();
+        }
+    }
+    if (_receiver.ackno().has_value() && (seg.length_in_sequence_space() == 0) &&
+        seg.header().seqno == _receiver.ackno().value() - 1) {
+        _sender.send_empty_segment();
+        should_sent = true;
+    }
+    // 有要发送的segment，把ack,window_size放在这个Segment中
+    if (should_sent) {
+        // TCPSegment &segment_sent = _sender.segments_out().front();
+        // _sender.segments_out().pop();
+        // // 携带上ackno & ACK flags
+        // if (_receiver.ackno().has_value()) {
+        //     segment_sent.header().ack = true;
+        //     segment_sent.header().ackno = _receiver.ackno().value();
+        // }
+        // segment_sent.header().win = _receiver.window_size() > std::numeric_limits<uint16_t>::max()
+        //                                 ? std::numeric_limits<uint16_t>::max()
+        //                                 : _receiver.window_size();
+        // _segments_out.push(segment_sent);
+        try_send();
+    }
+    // receiver.stream_out.eof ? end_input ?
+    if (!_sender.stream_in().eof() && _receiver.stream_out().eof()) {
+        _linger_after_streams_finish = false;
+    }
+}
+
+bool TCPConnection::active() const { return is_active; }
 
 size_t TCPConnection::write(const string &data) {
-    DUMMY_CODE(data);
-    return {};
+    size_t actual_written = _sender.stream_in().write(data);
+    // 尝试进行发送
+    _sender.fill_window();
+    if (!_sender.segments_out().empty()) {
+        // TCPSegment &segment_sent = _sender.segments_out().front();
+        // _sender.segments_out().pop();
+        // // 携带上ackno & ACK flags
+        // if (_receiver.ackno().has_value()) {
+        //     segment_sent.header().ack = true;
+        //     segment_sent.header().ackno = _receiver.ackno().value();
+        // }
+        // segment_sent.header().win = _receiver.window_size() > std::numeric_limits<uint16_t>::max()
+        //                                 ? std::numeric_limits<uint16_t>::max()
+        //                                 : _receiver.window_size();
+        // _segments_out.push(segment_sent);
+        try_send();
+    }
+    return actual_written;
 }
 
 //! \param[in] ms_since_last_tick number of milliseconds since the last call to this method
-void TCPConnection::tick(const size_t ms_since_last_tick) { DUMMY_CODE(ms_since_last_tick); }
+void TCPConnection::tick(const size_t ms_since_last_tick) {
+    // 告知TCPSender时间的流逝
+    _sender.tick(ms_since_last_tick);
+    try_send();
+    _time_since_last_segment_received += ms_since_last_tick;
+    // 重传次数超过max_retx_attempts -> abort the connection
+    if (_sender.consecutive_retransmissions() >= TCPConfig::MAX_RETX_ATTEMPTS) {
+        // abort the connection and send a reset segment to the peer
+        _sender.stream_in().set_error();
+        _receiver.stream_out().set_error();
+        is_active = false;
+        // 发送reset segment
+        _sender.send_empty_segment();
+        TCPSegment &segment_sent = _sender.segments_out().front();
+        _sender.segments_out().pop();
+        segment_sent.header().rst = true;
 
-void TCPConnection::end_input_stream() {}
+        _segments_out.push(segment_sent);
+    }
+    // Or cleanly end the connection
+    // 满足PREQ 1~3并且_linger = false 时，cleanDone
+    if (_receiver.stream_out().eof() && _sender.stream_in().eof() && !_sender.bytes_in_flight()) {
+        if (!_linger_after_streams_finish) {
+            is_active = false;
+        } else if (_time_since_last_segment_received >= 10 * _cfg.rt_timeout) {
+            is_active = false;
+        }
+    }
+}
 
-void TCPConnection::connect() {}
+void TCPConnection::end_input_stream() { 
+    _sender.stream_in().end_input();
+    // 尝试发送 fin
+    _sender.fill_window();
+    // TCPSegment &segment_sent = _sender.segments_out().front();
+    // _sender.segments_out().pop();
+    // // 携带上ackno & ACK flags
+    // if (_receiver.ackno().has_value()) {
+    //     segment_sent.header().ack = true;
+    //     segment_sent.header().ackno = _receiver.ackno().value();
+    // }
+    // segment_sent.header().win = _receiver.window_size() > std::numeric_limits<uint16_t>::max()
+    //                                 ? std::numeric_limits<uint16_t>::max()
+    //                                 : _receiver.window_size();
+    // _segments_out.push(segment_sent);
+    try_send();
+}
+
+void TCPConnection::connect() {
+    // Initiate a connection by sending a SYN segment
+    _sender.fill_window();
+    // std::queue<TCPSegment> &out_queue = _sender.segments_out();
+    // _segments_out.push(_sender.segments_out().front());
+    // out_queue.pop();
+    try_send();
+}
 
 TCPConnection::~TCPConnection() {
     try {
--- libsponge/tcp_connection.hh
+++ libsponge/tcp_connection.hh
@@ -20,7 +20,9 @@ class TCPConnection {
     //! for 10 * _cfg.rt_timeout milliseconds after both streams have ended,
     //! in case the remote TCPConnection doesn't know we've received its whole stream?
     bool _linger_after_streams_finish{true};
-
+    bool is_active{true};
+    size_t _time_since_last_segment_received{0};
+    void try_send(); // 如果sender_ out_sedments有待发送的segments，马上发送
   public:
     //! \name "Input" interface for the writer
     //!@{
